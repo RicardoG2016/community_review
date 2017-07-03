387befc498303f3375105e850ddbc4d2812ba63b
RicardoG2016 <gomez.r@live.com>
2017-07-03 16:30:51 -0500

    added defautl sidebar and login logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   acts_as_votable
   belongs_to :user
   has_many :comments
-  has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }, :default_url => "/public/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x250#", thumb: "100x100>" }, :default_url => "/public/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
